aa5346337708f6ad173bbd17922327d73daa2543
Kenneth Sheridan <kennethdsheridan@gmail.com>
2025-04-04 18:51:22 -0700

    added internal crate dependencies along with setting constants

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -1,11 +1,18 @@
 use crate::application::Application;
-use crate::ports::benchmark_port::BenchmarkPort;
+use crate::ports::benchmark_port::{
+    BenchmarkConfig, BenchmarkMetrics, BenchmarkParams, BenchmarkPort,
+    BenchmarkResult, BenchmarkTool, FIOParams, IOType,
+};
 use crate::ports::database_port::DatabasePort;
 use crate::ports::metrics_port::MetricsPort;
 
 use anyhow::Result;
 use colored::*;
 use dialoguer::{theme::ColorfulTheme, Confirm, Select};
+
+const BLOCK_SIZES: &[&str] = &["4k", "8k", "16k", "32k", "64k", "128k", "256k", "512k", "1m"];
+
+const BENCHMARK_DIR: &str = "/mnt/benchmark";
 
 pub fn run_benchmark<DB: DatabasePort, B: BenchmarkPort, M: MetricsPort>(
     app: &mut Application<DB, B, M>,
