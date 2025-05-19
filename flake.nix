d7d78ece3838d5dfacba4b366c823e9b13cf782d
Kenneth Sheridan <kennethdsheridan@gmail.com>
2025-05-19 12:49:17 -0700

    updated file for Linux

--- flake.nix
+++ flake.nix
@@ -1,23 +1,22 @@
-{
-  description = "sysperf-svr: Rust system performance service with .deb and .tar.gz outputs";
+ {
+  description = "sysperf-svr: Static musl Rust service with .deb and .tar.gz packaging";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlay ];
+        pkgs = import nixpkgs { inherit system; };
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = pkgs.cargo;
+          rustc = pkgs.rustc;
         };
 
-        rust = pkgs.rust-bin.stable.latest.default;
-
-        sysperf-svr = pkgs.rustPlatform.buildRustPackage {
+        sysperf-svr = rustPlatform.buildRustPackage {
           pname = "sysperf-svr";
           version = "0.1.0";
           src = ./.;
@@ -26,30 +25,35 @@
             lockFile = ./Cargo.lock;
           };
 
-          doCheck = false;
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.musl
+          ];
 
-          # Static build with musl
+          cargoBuildFlags = [
+            "--target=x86_64-unknown-linux-musl"
+          ];
+
           RUSTFLAGS = "-C target-feature=+crt-static";
-          buildPhase = ''
-            cargo build --release --target x86_64-unknown-linux-musl
-          '';
 
           installPhase = ''
             mkdir -p $out/bin
             cp target/x86_64-unknown-linux-musl/release/sysperf-svr $out/bin/
           '';
+
+          doCheck = false;
         };
 
         tarball = pkgs.runCommand "sysperf-svr-tarball" {
           nativeBuildInputs = [ pkgs.gzip ];
         } ''
           mkdir -p $out
-          mkdir -p build
-          cp ${sysperf-svr}/bin/sysperf-svr build/
-          tar -czf $out/sysperf-svr.tar.gz -C build sysperf-svr
+          cp ${sysperf-svr}/bin/sysperf-svr ./sysperf-svr
+          tar -czf $out/sysperf-svr.tar.gz sysperf-svr
         '';
 
         deb = pkgs.callPackage ./debian.nix { inherit sysperf-svr; };
+
       in {
         packages.default = deb;
         packages.tarball = tarball;
